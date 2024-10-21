950c3a3989c83f58e8dd790825decf553265c091
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2024-10-21 12:27:45 +0530

    crud operations added

--- NamestaSystemDesign/Networking/RestAPI/index.js
+++ NamestaSystemDesign/Networking/RestAPI/index.js
@@ -1,14 +1,81 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 const PORT = 4521;
 
-app.all('/', (req, res)=> {
-    res.send('Im Ups')
+let todos = [
+    {
+        id: '1',
+        title: "Todo 1",
+        completed: false
+    },
+    {
+        id: '2',
+        title: "Todo 2",
+        completed: true
+    },
+]
+
+// READ
+app.get('/todos', (req, res) => {
+    res.json(todos)
 })
 
-app.listen(PORT, ()=> {
+// CREATE
+app.post('/todos', (req, res) => {
+    const newTodo = req.body;
+    todos.push(newTodo);
+    res.json({
+        message: 'New Todo Added!'
+    });
+})
+
+// UPDATE
+app.put('/todos/:id', (req, res) => {
+    const newTodoData = req.body;
+    const todoIndex = req.params.id;
+    const todoDataIndex = todos.findIndex(item => {
+        return item.id === todoIndex
+    }
+    );
+    if (todoDataIndex !== -1) {
+        todos[todoDataIndex] = {
+            id: todoIndex,
+            ...newTodoData
+        }
+    }
+    res.json({
+        message: 'Todo Completed Successfully!'
+    })
+})
+
+// DELETE
+app.delete('/todos/:id', (req, res) => {
+    const deleteIndex = req.params.id;
+    // todos = todos.filter(item => item.id !== deleteIndex);
+    
+    // alter way to delete
+    const todoIndex = todos.findIndex((item) => item.id === deleteIndex);
+    if (todoIndex !== -1) {
+        todos.splice(todoIndex, 1);
+    }
+
+    res.json({
+        message: 'Todo deleted Successfully'
+    })
+})
+
+
+
+app.all('/', (req, res) => {
+    res.send('I am Home page')
+})
+
+app.listen(PORT, () => {
     console.log(`App is running in PORT ${PORT}`)
 });
 
