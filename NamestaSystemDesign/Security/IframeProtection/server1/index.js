218fe158604f18cc41163ea851a81a779e055141
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2024-11-20 08:24:47 +0530

    updates

--- NamestaSystemDesign/Security/IframeProtection/server1/index.js
+++ NamestaSystemDesign/Security/IframeProtection/server1/index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 
 // serve static files (optional)
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Define your routes
-app.get('example1', (req, res) => {
+app.get('/example1', (req, res) => {
+    console.log('check')
     res.sendFile(__dirname + '/public/example1.html');
 });
 
-app.get('example2', (req, res) => {
+app.get('/example2', (req, res) => {
     res.sendFile(__dirname + '/public/example2.html');
 });
 
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log('Port running in '+ port);
