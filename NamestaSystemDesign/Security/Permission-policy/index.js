ff61550e9d521bec2f92c748d1a6e5d9607f7bc5
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2024-11-29 20:41:30 +0530

    update

--- NamestaSystemDesign/Security/Permission-policy/index.js
+++ NamestaSystemDesign/Security/Permission-policy/index.js
@@ -65,8 +65,8 @@ app.get('/page', (req, res) => {
     `)
 })
 
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5010;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
