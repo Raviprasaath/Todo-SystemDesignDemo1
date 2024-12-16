d67597b8064f12059f7fa1587abcd7dd3791fc4c
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2024-12-16 09:36:48 +0530

    e2e update

--- NamestaSystemDesign/Testing/e2e.js
+++ NamestaSystemDesign/Testing/e2e.js
@@ -1,10 +1,33 @@
 const puppeteer = require('puppeteer');
 
 // Immediately invoke function expression
-(()=>{
+// (()=>{
 
+// })();
+
+// (function(){
+
+// }())
+
+
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--window-size=1680,1050"],
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://www.google.com/");
+
+    console.log('Webpage loaded');
+
+    await page.setViewport({ width: 1680, height: 1050 });
+
+    const googleClickButton = "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.RNmpXc";
+
+    await page.waitForSelector(googleClickButton);
+
+    await page.click(googleClickButton)
 })();
-
-(function(){
-
-}())
