bbf4abe3e67b49726cc7fc183db7544d039c02da
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2024-12-18 09:24:29 +0530

    tdd update

---
+++ NamestaSystemDesign/Testing/tdd.test.js
@@ -0,0 +1,11 @@
+// Palindrome Check
+
+// abc -> false
+// aba -> true
+// no input -> null
+// null -> null
+// single letter -> always true
+// 123 -> false
+// 121 -> true
+// Negative Number -> -121 -> true
+// Boolean, {}, [], ()=>{} -> true
